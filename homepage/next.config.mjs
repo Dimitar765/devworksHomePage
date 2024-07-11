e3d17fa1b6d842de6e15af74419cc62996734eed
Dimitar765 <112801140+Dimitar765@users.noreply.github.com>
2024-07-11 19:17:07 +0200

    about page, design component

--- homepage/next.config.mjs
+++ homepage/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["blog.thenounproject.com"],
+  },
+};
 
 export default nextConfig;
